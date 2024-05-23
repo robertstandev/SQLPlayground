fda06622b49924d96e39a82e9b783f7237b14013
Robert Stan <robertstan1991@gmail.com>
2024-05-23 09:36:55 +0300

    Update 0Template Clean Code.sql

--- CheatSheet/Other/0Template Clean Code.sql
+++ CheatSheet/Other/0Template Clean Code.sql
@@ -1,139 +1,93 @@
-/****** Object:  StoredProcedure [API].[sp_OrdersIn_Add]    Script Date: 8/10/2023 4:27:22 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
-ALTER PROCEDURE [API].[sp_OrdersIn_Add]
-	@Parameters NVARCHAR(MAX),
-	@Response NVARCHAR(MAX) OUT,
+ALTER PROCEDURE [dbo].[sp_YourProcedureName]
+	@Data NVARCHAR(1000),
+	@PersonID INT = 1,
 	@Log NVARCHAR(MAX) = NULL OUT,
 	@Forced BIT = 0,
 	@Debug BIT = 0
 AS
----================================
---- Write here what the procedure does and what parameter it has
----================================
 BEGIN
 	SET NOCOUNT ON;
-
-	/* Declare */
-	BEGIN
-		DECLARE @ProcedureStartDate DATETIME = dbo.GETDATE2()
-		DECLARE @TranCount INT = @@TRANCOUNT
-		DECLARE @ProcedureName VARCHAR(250) = OBJECT_SCHEMA_NAME(@@PROCID) + '.' + OBJECT_NAME(@@PROCID)
-		DECLARE @TranName VARCHAR(250) = CONCAT(@ProcedureName, NEWID())
-	END--End declare
-
-	/* Start procedure debug */
-	SET @Log = CONCAT(@Log, [dbo].[fx_DebugLine](@@PROCID, '0000' , ''))
 	
-	---=====================================================================
-	BEGIN TRY
-
-		IF @TranCount > 0
-			SAVE TRAN @TranName
-		ELSE 
-			BEGIN TRAN
-
-		SET @Log = CONCAT(@Log, [dbo].[fx_DebugLine](@@PROCID, '1000' , ''))
-
-		/* Write your code here */
-
-		INSERT INTO dbo.tblDebugging 
-			(Description,
-			LogText)
-		SELECT TOP(1)
-			[Description] = @ProcedureName
-			,[LogText] = @Parameters
-			
-
-		IF @trancount = 0
-			COMMIT TRAN
-	END TRY
-	---=====================================================================
-
-	---=====================================================================
-	BEGIN CATCH
-		IF XACT_STATE() = -1 OR XACT_STATE() = 1 AND @TranCount = 0
-			ROLLBACK TRAN
-		IF XACT_STATE() = 1 AND @TranCount > 0
-			ROLLBACK TRAN @TranName;
-
-		/* Error debug */
-		SET @Log = CONCAT(@Log, [dbo].[fx_DebugLine](@@PROCID, '9998' , ''))
-
-		EXEC [dbo].[sp_LogError] 
-			@ErrorType = 'ERROR',
-			@ErrorSeverity = NULL,
-			@ErrorSource = NULL, 
-			@ErrorDetails = NULL, --OUT
-			@PersonID = NULL,
-			@ForceLog = 0,
-			@PrintError = 0,
-			@OrderID = NULL,
-			@OrderNumber = NULL,
-			@ErrorSuffix = '',
-			@ProcID = @@PROCID,
-			@RaiseError = 1,
-			@SendEmail = 0,
-			@TaskID = NULL,
-			@TranslateError = 0,
-			@CheckContextValues = 0,
-			@Log = @Log OUT,
-			@Forced = @Forced,
-			@Debug = @Debug
-	END CATCH
-	---=====================================================================
-
-	/* End procedure debug */
-	SET @Log = CONCAT(@Log, [dbo].[fx_DebugLine](@@PROCID, '9999' , ''))
-
-	---=====================================================================
-	---Log high procedure execution time
-	---=====================================================================
-	BEGIN 
-		--get the total execution time of the procedure
-		DECLARE @ProcedureEndDate DATETIME = dbo.GETDATE2()
-		DECLARE @ProcedureExecuteDurationMilliseconds INT = DATEDIFF(MILLISECOND, @ProcedureStartDate, @ProcedureEndDate)
-
-		--log the total execution time if above the settings treshold
-		IF @ProcedureExecuteDurationMilliseconds > dbo.fx_Tools_DebuggingTime()
-		BEGIN
-			INSERT INTO [dbo].[tblDebugging]
-				   ([ItemID]
-				   ,[idOrder]
-				   ,[idTask]
-				   ,[idAdmPerson]
-				   ,[Duration]
-				   ,[BeginDate]
-				   ,[EndDate]
-				   ,[Description]
-				   ,[Description2]
-				   ,[Description3]
-				   ,[Description4]
-				   ,[Description5]
-				   ,[LogText]
-				   ,[SourceName]
-				   ,[Errors])
-			 SELECT TOP(1)
-					[ItemID] = NULL --Here add ProcessType, EntityID, OrderID, etc..
-				   ,[idOrder] = NULL
-				   ,[idTask] = NULL
-				   ,[idAdmPerson] = NULL
-				   ,[Duration] = @ProcedureExecuteDurationMilliseconds
-				   ,[BeginDate] = @ProcedureStartDate
-				   ,[EndDate] = @ProcedureEndDate
-				   ,[Description] = 'High execution time'
-				   ,[Description2] = ''
-				   ,[Description3] = ''
-				   ,[Description4] = ''
-				   ,[Description5] = ''
-				   ,[LogText] = ISNULL(@Log, '')
-				   ,[SourceName] = @ProcedureName
-				   ,[Errors] = ERROR_MESSAGE()
-		END
+	--DECLARATIONS
+	BEGIN
+		DECLARE @ProcedureSchema VARCHAR(10) = OBJECT_SCHEMA_NAME(@@PROCID)
+		DECLARE @ProcedureName VARCHAR(100) = OBJECT_NAME(@@PROCID)
+		DECLARE @trancount INT = @@TRANCOUNT
+		DECLARE @tranName VARCHAR(1000) = ISNULL(@ProcedureName + '-', '') + CAST(NEWID() AS VARCHAR(100))
+		
+		--Other variables
 	END
-	---=====================================================================
-END
+	
+	IF @trancount > 0
+		SAVE TRAN @tranName
+	ELSE 
+		BEGIN TRAN
+	BEGIN TRY
+        IF EXISTS (
+            SELECT 1
+            FROM sys.procedures p WITH(NOLOCK)
+            WHERE p.name = @ProcedureName
+                AND p.schema_name = 'CUST'
+            ) 
+        AND @ProcedureSchema <> 'CUST'
+        BEGIN
+            EXEC [CUST].@ProcedureName @Data = @Data, @PersonID = @PersonID, @Forced = @Forced, @Log = @Log OUT; -- Execute CUST schema procedure
+        END
+        ELSE
+        BEGIN
+        	--Initialize Variables
+		BEGIN
+			PRINT 'Do selects here with joins to get the data that you want to check for / filter for in the above code like = get if personid is admin or get if orderid is idorderin etc'
+		END
 
+        
+            IF @Debug = 1
+                PRINT '100: ' + CONVERT(VARCHAR(100), dbo.GETDATE2(), 114) + ' --> ' + @ProcedureSchema + '.' + @ProcedureName;
+
+
+            EXEC dbo.sp_Event_Before
+                @Data = @Data, 
+                @PersonID = @PersonID, 
+                @Forced = @Forced,
+                @Debug = @Debug
+
+            /*
+
+            Your code here
+
+
+            */
+
+            EXEC dbo.sp_Event_After
+                @Data = @Data, 
+                @PersonID = @PersonID, 
+                @Forced = @Forced,
+                @Debug = @Debug,
+                @Log = @Log OUT
+
+            IF @Debug = 1
+                PRINT '900: ' + CONVERT(VARCHAR(100), dbo.GETDATE2(), 114) + ' --> ' + @ProcedureSchema + '.' + @ProcedureName;
+        END
+    END TRY  -- `COMMIT TRAN` should be here
+    BEGIN CATCH
+        IF XACT_STATE() = -1 OR XACT_STATE() = 1 and @trancount = 0
+            ROLLBACK TRAN
+        IF XACT_STATE() = 1 and @trancount > 0
+            ROLLBACK TRAN @tranName;
+        
+        EXEC [dbo].[sp_LogError] 
+            @ProcID = @@PROCID, 
+            @RaiseError = 1, 
+            @Debug = @Debug, 
+            @OrderID = @OrderInID, 
+            @ErrorSuffix = @OrderInState, 
+            @Forced = @Forced, 
+            @Log = @Log OUT, 
+            @PersonID = @PersonID;
+            -- Consider adding more specific error handling here (e.g., RAISERROR)
+    END CATCH
+END
